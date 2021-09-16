aba2c7f19533e616c97c9074c6a4fc5dc76b1b77
Tonny221 <anthonny14gabriell@gmail.com>
2021-09-16 19:46:40 -0400

    global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
+
+import { GlobalStyles } from './GlobalStyles';
+
 function App(){
   return(
-    <div>Hello World</div>
+    <div>
+      Hello World
+      <GlobalStyles />
+    </div>
   );
 }
 
